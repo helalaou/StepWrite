fadbb37a9118dacc4960f07c68b1d9af22351c6f
Hamza El Alaoui <helalaou@cs.cmu.edu>
2025-03-13 01:27:51 -0400

    chore: reconfigure vad

--- client/src/config.js
+++ client/src/config.js
@@ -60,7 +60,7 @@ const config = {
       preSpeechPadFrames: 10,          // Range: 1-10, Frames to keep before speech 
       positiveSpeechThreshold: 0.7,  // Range: 0.5-0.95, Confidence for positive detection 
       negativeSpeechThreshold: 0.60,  // Range: 0.5-0.90, Confidence for negative detection
-      redemptionFrames: 6,            // Range: 1-10, Frames to wait before confirming end 
+      redemptionFrames: 12,            // Range: 1-10, Frames to wait before confirming end 
       mode: 3,                        // Range: 0-3, VAD aggressiveness (3 = most aggressive)
     },
     
